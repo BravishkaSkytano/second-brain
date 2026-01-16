c5c9592582b5ea44f50be19f46e3257f9e4f70e6
BravishkaSkytano <65601594+BravishkaSkytano@users.noreply.github.com>
2026-01-16 02:00:55 +0000

    Jan 15, 2026, 9:00 PM

--- eleventy.config.js
+++ eleventy.config.js
@@ -129,7 +129,6 @@ export default async function(eleventyConfig) {
 
 	// eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
 
-
 	// Interlinker
 	eleventyConfig.addPlugin(InterlinkerPlugin, {
 		defaultLayout: 'layouts/embed.liquid'
@@ -139,26 +138,6 @@ export default async function(eleventyConfig) {
 		html: true,
 		linkify: true,
 		typographer: true
-	})
-	
-	// Recognize Mediawiki links ([[text]])
-	md.linkify.add("[[", {
-		validate: /^\s?([^\[\]\|\n\r]+)(\|[^\[\]\|\n\r]+)?\s?\]\]/,
-		normalize: match => {
-			const parts = match.raw.slice(2, -2).split("|");
-			const slug = slugify(parts[0].replace(/.(md|markdown)\s?$/i, "").trim());
-			const found = linkMapCache.get(slug);
-
-			if (!found) throw new Error(`Unable to find page linked by wikilink slug [${slug}]`)
-
-			match.text = parts.length === 2
-				? parts[1]
-				: found.title;
-
-			match.url = found.permalink.substring(0, 1) === '/'
-				? found.permalink
-				: `/${found.permalink}`;
-		}
 	})
 };
 
