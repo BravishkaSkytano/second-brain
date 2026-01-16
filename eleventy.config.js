45f5e5377364bca2aeed843196f69608af35fa8b
BravishkaSkytano <65601594+BravishkaSkytano@users.noreply.github.com>
2026-01-16 01:49:55 +0000

    Jan 15, 2026, 8:49 PM

--- eleventy.config.js
+++ eleventy.config.js
@@ -135,26 +135,31 @@ export default async function(eleventyConfig) {
 		defaultLayout: 'layouts/embed.liquid'
 	});
 
-	eleventyConfig.setLibrary("md", markdownIt());
-  // Recognize Mediawiki links ([[text]])
-  md.linkify.add("[[", {
-    validate: /^\s?([^\[\]\|\n\r]+)(\|[^\[\]\|\n\r]+)?\s?\]\]/,
-    normalize: match => {
-      const parts = match.raw.slice(2, -2).split("|");
-      const slug = slugify(parts[0].replace(/.(md|markdown)\s?$/i, "").trim());
-      const found = linkMapCache.get(slug);
+	const md = markdownIt({
+		html: true,
+		linkify: true,
+		typographer: true
+	})
+	
+	// Recognize Mediawiki links ([[text]])
+	md.linkify.add("[[", {
+		validate: /^\s?([^\[\]\|\n\r]+)(\|[^\[\]\|\n\r]+)?\s?\]\]/,
+		normalize: match => {
+			const parts = match.raw.slice(2, -2).split("|");
+			const slug = slugify(parts[0].replace(/.(md|markdown)\s?$/i, "").trim());
+			const found = linkMapCache.get(slug);
 
-      if (!found) throw new Error(`Unable to find page linked by wikilink slug [${slug}]`)
+			if (!found) throw new Error(`Unable to find page linked by wikilink slug [${slug}]`)
 
-      match.text = parts.length === 2
-        ? parts[1]
-        : found.title;
+			match.text = parts.length === 2
+				? parts[1]
+				: found.title;
 
-      match.url = found.permalink.substring(0,1) === '/'
-        ? found.permalink
-        : `/${found.permalink}`;
-			}
-		})
+			match.url = found.permalink.substring(0, 1) === '/'
+				? found.permalink
+				: `/${found.permalink}`;
+		}
+	})
 
 	// This regex finds all wikilinks in a string
 	const wikilinkRegExp = /(?<!!)\[\[([^|]+?)(\|([\s\S]+?))?\]\]/g;
@@ -168,63 +173,63 @@ export default async function(eleventyConfig) {
 			link,
 			slug
 		}
-	});
+	})
 
 	// This function gets past each page via *.11tydata.js in order to
 	// fill that pages backlinks data array.
 	if (!data.collections.all || data.collections.all.length === 0) return [];
-  const allPages = data.collections.all;
-  const currentSlug = slugify(data.title);
-  let backlinks = [];
-  let currentSlugs = [currentSlug];
+	const allPages = data.collections.all;
+	const currentSlug = slugify(data.title);
+	let backlinks = [];
+	let currentSlugs = [currentSlug];
 
-  // Populate our link map for use later in replacing wikilinks with 
-  // page permalinks.
-  // Pages can list aliases in their front matter, if those exist we
-  // should map them as well.
+	// Populate our link map for use later in replacing wikilinks with 
+	// page permalinks.
+	// Pages can list aliases in their front matter, if those exist we
+	// should map them as well.
 
-  linkMapCache.set(currentSlug, {
-    permalink: data.permalink,
-    title: data.title
-  });
+	linkMapCache.set(currentSlug, {
+		permalink: data.permalink,
+		title: data.title
+	});
 
-  if (data.aliases) {
-    for(const alias of data.aliases) {
-      const aliasSlug = slugify(alias);
-      linkMapCache.set(aliasSlug, {
-        permalink: data.permalink,
-        title: alias
-      });
-      currentSlugs.push(aliasSlug)
-    }
-  }
+	if (data.aliases) {
+		for(const alias of data.aliases) {
+			const aliasSlug = slugify(alias);
+			linkMapCache.set(aliasSlug, {
+				permalink: data.permalink,
+				title: alias
+			});
+			currentSlugs.push(aliasSlug)
+		}
+	}
 
-  // Loop over all pages and build their outbound links if they
-  // have not already been parsed, this is being done in a way 
-  // that is cached between reloads so restarting the dev server
-  // will be required to pick up changes.
-  
-  allPages.forEach(page => {
-    if (!page.data.outboundLinks) {
-      const pageContent = page.template.frontMatter.content;
-      const outboundLinks = (pageContent.match(wikilinkRegExp) || []);
-      page.data.outboundLinks = parseWikilinks(outboundLinks);
-    }
+	// Loop over all pages and build their outbound links if they
+	// have not already been parsed, this is being done in a way 
+	// that is cached between reloads so restarting the dev server
+	// will be required to pick up changes.
+	
+	allPages.forEach(page => {
+		if (!page.data.outboundLinks) {
+			const pageContent = page.template.frontMatter.content;
+			const outboundLinks = (pageContent.match(wikilinkRegExp) || []);
+			page.data.outboundLinks = parseWikilinks(outboundLinks);
+		}
 
-    // If the page links to our current page either by its title
-    // or by its aliases then add that page to our current 
-    // page's backlinks.
-    
-    if (page.data.outboundLinks.some(link => currentSlugs.includes(link.slug))) {
-      backlinks.push({
-        url: page.url,
-        title: page.data.title,
-      })
-    }
-  });
+		// If the page links to our current page either by its title
+		// or by its aliases then add that page to our current 
+		// page's backlinks.
+		
+		if (page.data.outboundLinks.some(link => currentSlugs.includes(link.slug))) {
+			backlinks.push({
+				url: page.url,
+				title: page.data.title,
+			})
+		}
+	});
 
-  // The backlinks for the current page, set to the page data
-  return backlinks;
+	// The backlinks for the current page, set to the page data
+	return backlinks;
 };
 
 export const config = {
